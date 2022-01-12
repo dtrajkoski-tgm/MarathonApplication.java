a3e14a3c15e07346c1dac526b8c7d39012e7d51a
dtrajkoski-tgm <dtrajkoski@student.tgm.ac.at>
2022-01-12 22:25:25 +0100

    final

--- src/main/java/sectionpark/model/TimingstationData.java
+++ src/main/java/sectionpark/model/TimingstationData.java
@@ -14,21 +14,20 @@ public class TimingstationData {
 	private double altitude;
 	private String unitAltitude;
 
-	private WeatherData weather;
-
-	private CompetitionData competition;
-
 
 	/**
 	 * Constructor
 	 */
 	public TimingstationData() {
+
 		this.timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
-		this.unitDistance = "km";
-		this.unitAltitude = "m";
-		this.weather = new WeatherData();
-		this.competition = new CompetitionData();
+		this.unitDistance = "m";
+		this.unitAltitude = "hm";
 	}
+
+
+
+
 
 	/**
 	 * Setter and Getter Methods
@@ -41,7 +40,9 @@ public class TimingstationData {
 		this.timingstationID = timingstationID;
 	}
 
-	public String getTimestamp() { return this.timestamp; }
+	public String getTimestamp() {
+		return timestamp;
+	}
 
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
@@ -51,120 +52,25 @@ public class TimingstationData {
 		this.distance = distance;
 	}
 
-	public double getDistance() { return distance; }
-
-	public double getAltitude() { return altitude; }
-
-	public void setAltitude(double altitude) { this.altitude = altitude;}
-
-	public WeatherData getWeather() { return this.weather; }
-
-	public void setWeather(WeatherData wd){ this.weather = wd; }
-
-	public CompetitionData getCompetition() { return this.competition; }
-
-	public void setCompetition(CompetitionData cd){ this.competition = cd; }
-
-	public String getUnitDistance() { return unitDistance; }
-
-	public String getUnitAltitude() { return unitAltitude; }
-
-	/**
-	 * class for the WeatherData in TimingStation
-	 */
-	public static class WeatherData {
-
-		private double windspeed;
-		private String unitWindspeed;
-
-		private double temperature;
-		private String unitTemperature;
-
-		private double precipitation;
-		private String unitPrecipitation;
-
-		private double atmosphericPressure;
-		private String unitAtmosphericPressure;
-
-		public WeatherData() {
-			this.unitWindspeed = "kmH";
-			this.unitTemperature = "°C";
-			this.unitPrecipitation = "ml";
-			this.unitAtmosphericPressure = "hPa";
-		}
-
-		public double getWindspeed() { return windspeed; }
-
-		public void setWindspeed(double windspeed) { this.windspeed = windspeed; }
-
-		public String getUnitWindspeed() { return unitWindspeed; }
-
-		public double getTemperature() { return this.temperature; }
-
-		public void setTemperature(double temperature) { this.temperature = temperature; }
-
-		public String getUnitTemperature() { return unitTemperature; }
-
-		public double getPrecipitation() { return this.precipitation; }
-
-		public void setPrecipitation(double precipitation) { this.precipitation = precipitation; }
-
-		public String getUnitPrecipitation() { return unitPrecipitation; }
-
-		public double getAtmosphericPressure() { return this.atmosphericPressure; }
-
-		public void setAtmosphericPressure(double atmosphericPressure){ this.atmosphericPressure = atmosphericPressure; }
-
-		public String getUnitAtmosphericPressure() { return unitAtmosphericPressure; }
+	public double getDistance() {
+		return distance;
 	}
 
-	/**
-	 * class for the CompetitionData in TimingStation
-	 */
-	public static class CompetitionData {
-
-		private String unitTiming;
-		private Party[] party;
-
-		public CompetitionData() {
-			this.unitTiming = "hh::mm::ss";
-			this.party = new Party();
-		}
-
-		public String getUnitTiming(){ return this.unitTiming; }
-
-		public Party[] getParty(){ return this.party; }
-
-		public void setParty(Party[] party){ this.party = party; }
-
-		public static class Party {
-
-			private int partyID;
-			private String timing;
-
-			public Party() {
-				this.partyID = 0;
-				this.timing = "";
-			}
-
-			public int getPartyID(){ return this.partyID; }
-
-			public void setPartyID(int partyID){ this.partyID = partyID; }
-
-			public String getTiming(){ return this.timing; }
-
-			public void setTiming(String timing){ this.timing += timing; }
-
-			int i = 0;
-		}
+	public double getAltitude() {
+		return altitude;
 	}
+
+	public void setAltitude(double altitude) {
+		this.altitude = altitude;
+	}
+
 	/**
 	 * Methods
 	 */
 	@Override
 	public String toString() {
-		String info = String.format("Timing Station Info: ID = %s, timestamp = %s, distance = %d", 
-			timingstationID, timestamp, distance );
+		String info = String.format("Timing Station Info: ID = %s, timestamp = %s, distance = %d",
+				timingstationID, timestamp, distance );
 		return info;
 	}
 }
--- src/main/java/sectionpark/timingstation/TimingstationSimulation.java
+++ src/main/java/sectionpark/timingstation/TimingstationSimulation.java
@@ -1,51 +1,122 @@
 package sectionpark.timingstation;
 
-import sectionpark.model.TimingstationData;
+import sectionpark.model.*;
 
 public class TimingstationSimulation {
-	
+
 	private double getRandomDouble( int inMinimum, int inMaximum ) {
 
-		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum; 
-		double rounded = Math.round(number * 100.0) / 100.0; 
+		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum;
+		double rounded = Math.round(number * 100.0) / 100.0;
 		return rounded;
-		
+
 	}
 
 	private int getRandomInt( int inMinimum, int inMaximum ) {
 
-		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum; 
-		Long rounded = Math.round(number); 
+		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum;
+		Long rounded = Math.round(number);
 		return rounded.intValue();
 
 	}
-	
+
 	public TimingstationData getData( String inTimingstationID ) {
+
+		// Generator
+
 		TimingstationData data = new TimingstationData();
 		data.setTimingstationID( inTimingstationID );
-		data.setDistance(getRandomInt(1,45));
-		data.setAltitude( getRandomInt(100,250) );
-		TimingstationData.WeatherData weather = new TimingstationData.WeatherData();
-		weather.setPrecipitation(0);
-		weather.setTemperature(getRandomInt(15,30));
-		weather.setAtmosphericPressure(getRandomInt(100,101));
-		weather.setWindspeed(getRandomInt(10,40));
-		data.setWeather(weather);
-		int i = getRandomInt(1,4)
-		TimingstationData.CompetitionData competition = new TimingstationData.CompetitionData[];
-		TimingstationData.CompetitionData.Party[] party = new TimingstationData.CompetitionData.Party[temp];
-		for(int i = 0; i < temp; i++) {
-			party[i] = new TimingstationData.CompetitionData.Party();
-			party[i].setPartyID(getRandomInt(0, 2000));
-			party[i].setTiming("00:" + getRandomInt(25, 50) + ":" + getRandomInt(25, 50));
-			competition.setParty(party);
-		}
-		data.setCompetition(competition);
-		cd.setParty();
+		data.setDistance( getRandomDouble(1, 40) );
+		data.setAltitude( getRandomDouble(100, 200) );
 
 
 		return data;
-		
+
 	}
+
+	public TimingstationJSON getJSON( String inTimingstationID ) {
+
+		// Generator
+
+		TimingstationJSON data = new TimingstationJSON();
+		data.setTimingstationID( inTimingstationID );
+		data.setDistance( getRandomDouble(1, 40) );
+		data.setAltitude( getRandomDouble(100, 200) );
+
+		data.getWeather().setWindspeed( getRandomDouble(0, 30) );
+		data.getWeather().setTemperature( getRandomDouble(20, 30));
+		data.getWeather().setPrecipitation( getRandomDouble(0, 5) );
+		data.getWeather().setAtmosphericPressure( getRandomDouble(700, 1000) );
+
+		TimingstationJSON.CompetitionData comp = new TimingstationJSON.CompetitionData( getRandomInt(1,6) );
+		TimingstationJSON.CompetitionData.Party[] partys = comp.getParty();
+		for (int i = 0; i < partys.length; i++) {
+			partys[i].setTiming("00:" + getRandomInt(15,30) + ":" + getRandomInt(0,59));
+		}
+
+		data.setCompetition(comp);
+
+		return data;
+
+	}
+
+	public TimingstationXML getXML( String inTimingstationID ) {
+
+		TimingstationXML data = new TimingstationXML();
+		data.setTimingstationID( inTimingstationID );
+		data.setDistance( getRandomDouble(1, 40) );
+		data.setAltitude( getRandomDouble(100, 200) );
+		data.getWeather().setWindspeed( getRandomDouble(0, 30) );
+		data.getWeather().setTemperature( getRandomDouble(20, 30));
+		data.getWeather().setPrecipitation( getRandomDouble(0, 5) );
+		data.getWeather().setAtmosphericPressure( getRandomDouble(700, 1000) );
+
+		TimingstationXML.CompetitionData comp = new TimingstationXML.CompetitionData( getRandomInt(1,6) );
+		TimingstationXML.CompetitionData.Party[] partys = comp.getParty();
+		for (int i = 0; i < partys.length; i++) {
+			partys[i].setTiming("00:" + getRandomInt(15,30) + ":" + getRandomInt(0,59));
+		}
+
+		data.setCompetition(comp);
+
+		return data;
+
+	}
+
+	/**
+	 * <timingstationData>
+	 *    <timingstationID>001</timingstationID>
+	 *    <timestamp>2021-09-12 08:52:39.077</timestamp>
+	 *    <distance>1</distance>
+	 *    <unitDistance>km</unitDistance>
+	 *    <altitude>177</altitude>
+	 *    <unitAltitude>m</unitAltitude>
+	 *    <weatherData>
+	 *     <windspeed>5.46</windspeed>
+	 *     <unitWindspeed>kmH</unitWindspeed>
+	 *     <temperature>25.78</temperature>
+	 *     <unitTemperature>C</unitTemperature>
+	 *     <precipitation>0</precipitation>
+	 *     <unitPrecipitation>ml</unitPrecipitation>
+	 *     <atmosphericPressure>999.0</atmosphericPressure>
+	 *     <unitAtmosphericPressure>hPa</unitAtmosphericPressure>
+	 *    </weatherData>
+	 *    <competitionData>
+	 *      <unitTiming>hh:mm:ss</unitTiming>
+	 *      <party>
+	 *        <partyID>1587</partyID>
+	 *        <timing>00:25:33</timing>
+	 *      </party>
+	 *      <party>
+	 *        <partyID>1437</partyID>
+	 *        <timing>00:27:01</timing>
+	 *      </party>
+	 *      <party>
+	 *        <partyID>1222</partyID>
+	 *        <timing>00:28:59</timing>
+	 *      </party>
+	 *    </competitionData>
+	 *  </timingstationData>
+	 */
 
 }
